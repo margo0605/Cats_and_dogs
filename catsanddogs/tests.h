2e5547821042b143614eb954987d8fa89e72292e
margo0605 <90922394+margo0605@users.noreply.github.com>
2022-06-28 23:11:00 +0300

    Add files via upload

--- catsanddogs/tests.h
+++ catsanddogs/tests.h
@@ -7,6 +7,10 @@
 
 #include <QObject>
 #include <QtTest/QtTest>
+#include <time.h>
+#include <QTimer>
+#include <QFontDatabase>
+#include <fstream>
 
 /*!
  * \brief Тестирование
@@ -37,6 +41,84 @@ private
     void basicComparison2() {
         QVERIFY(myCondition());
         QVERIFY(1 != 2);
+    }
+
+    void timer() {
+        timer = new QTimer(this);
+        QVERIFY(timer != null);
+    }
+
+    void checking_fonts() {
+        int id = QFontDatabase::addApplicationFont(":/fonts/After Cheret Bold.ttf");
+        QVERIFY(id != 0);
+
+        int i = QFontDatabase::addApplicationFont(":/fonts/Dynasty.ttf");
+        QVERIFY(id != 0);
+    }
+
+    void map_generator() {
+        for (int r = 0; r < 5; r++) {
+            for (int c = 0; c < 5; c++) {
+                GameField[r][c] = '-';
+            }
+        }
+
+        bool flag = false;
+
+        for (int r = 0; r < 5; r++) {
+            for (int c = 0; c < 5; c++) {
+                if (GameField[r][c]) != '-' {
+                    flag = true
+                }
+            }
+        }
+
+        char Field[5][5] = {
+                {'-','-','-','-','-'},
+                {'-','-','-','-','-'},
+                {'-','-','-','-','-'},
+                {'-','-','-','-','-'},
+                {'-','-','-','-','-'},
+        };
+        QVERIFY(Field == GameField);
+        QVERIFY(flag != true);
+    }
+
+    void filesChecker() {
+        std::ofstream out;
+
+        out.open("fonts/After Cheret Bold.ttf");
+        out.close();
+        QVERIFY(out != null);
+
+        out.open("fonts/Dynasty.ttf");
+        out.close();
+        QVERIFY(out != null);
+
+        out.open("pic/pngwing.com-3.png");
+        out.close();
+        QVERIFY(out != null);
+
+        out.open("pic/pngwing.com-6.png");
+        out.close();
+        QVERIFY(out != null);
+
+        out.open("pic/pngwing.com-9.png");
+        out.close();
+        QVERIFY(out != null);
+
+        out.open("pic/pngwing.com-11.png");
+        out.close();
+        QVERIFY(out != null);
+
+        out.open("pic/pngwing.com-12.png");
+        out.close();
+        QVERIFY(out != null);
+
+        out.open("pic/pngwing.com-13.png");
+        out.close();
+        QVERIFY(out != null);
+
     }
 
     void playerLock() {
